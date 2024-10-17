2b31248712c7beb513d8c3ec6d010c7c79d46d09
SumitMore_ <145145478+480147530332@users.noreply.github.com>
2024-10-17 20:34:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let menulist = document.getElementById("menulist")
+menulist.style.maxHeight="0px";
+
+
+
+function toogle(){
+    if(menulist.style.maxHeight=="0px")
+        {
+        menulist.style.maxHeight="300px";
+    }
+    else{
+        menulist.style.maxHeight="0px";
+    }
+}
+
+
+
+
+
+
+
